80223b88376fbf29e1e066a0fe8f607b5e6df9fd
Akash Sharma <akashsharma15105@gmail.com>
2022-10-22 17:49:11 +0530

    Added: Home Page

---
+++ src/common/constants/index.ts
@@ -0,0 +1,22 @@
+export const mailId: string = "akashsharma15105@gmail.com";
+export const githubUrl: string = "https://github.com/6102hsaka";
+export const linkedInUrl: string =
+    "https://www.linkedin.com/in/akash-sharma-a09016162/";
+export const leetcodeUrl: string = "https://leetcode.com/akashsharma15105/";
+export const hackerRankUrl: string =
+    "https://www.hackerrank.com/akashsharma15105";
+export const resumeUrl: string =
+    "https://drive.google.com/file/d/1RIm1JiEtqsKphXtKiJtvbfs-VIm_RExa/view?usp=sharing";
+
+export const keySkills: string[] = [
+    "JavaScript",
+    "React JS",
+    "Redux, Redux-saga",
+    "Jest / Enzyme",
+    "Java",
+    "Springboot",
+    "PostgreSQL",
+    "Firebase",
+    "Power BI",
+    "GIT",
+];
---
+++ src/components/home/index.tsx
@@ -0,0 +1,75 @@
+import { BsStars } from "react-icons/bs";
+import { SiGithub, SiLeetcode, SiLinkedin } from "react-icons/si";
+
+import {
+    githubUrl,
+    linkedInUrl,
+    leetcodeUrl,
+    resumeUrl,
+    keySkills,
+} from "../../common/constants";
+import "./home.scss";
+
+const Home = () => {
+    return (
+        <section className="home-container">
+            <div className="info">
+                <div className="links">
+                    <a
+                        href={linkedInUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="linkedIn"
+                    >
+                        <SiLinkedin className="link-icons" />
+                    </a>
+                    <a
+                        href={githubUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="github"
+                    >
+                        <SiGithub className="link-icons" />
+                    </a>
+                    <a
+                        href={leetcodeUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                        aria-label="leetcode"
+                    >
+                        <SiLeetcode className="link-icons" />
+                    </a>
+                </div>
+                <span id="name">Akash Sharma</span>
+                <p id="description">
+                    I'm coder and professional web developer with ability to
+                    manage multiple priorities
+                </p>
+                <a
+                    id="resume"
+                    href={resumeUrl}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Download Resume
+                </a>
+            </div>
+            <div className="skills">
+                <div className="skills-background"></div>
+                <div className="skills-front">
+                    <span id="key-skills-header">
+                        Key Skills
+                        <BsStars className="star-icon" />
+                    </span>
+                    <ul>
+                        {keySkills.map((keyskill) => (
+                            <li key={keyskill}>{keyskill}</li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
