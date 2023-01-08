3cfe08a54daf65b0e64d80c9f8ae2983749a0e9c
Akash Sharma <akashsharma15105@gmail.com>
2023-01-08 13:19:43 +0530

    Added: gatsby plugin for sitemap.xml and robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,58 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-    plugins: [`gatsby-plugin-sass`],
+    siteMetadata: {
+        title: `Akash Sharma Porfolio`,
+        description: `Akash Sharma Portfolio webapp build using react js, gatsby, sass, firebase`,
+        siteUrl: `https://akash-sharma-portfolio.web.app/`,
+    },
+    plugins: [
+        `gatsby-plugin-sass`,
+        {
+            resolve: "gatsby-plugin-sitemap",
+            options: {
+                query: `{
+                    site {
+                      siteMetadata {
+                        siteUrl
+                      }
+                    }
+                    allSitePage {
+                      nodes {
+                        path
+                      }
+                    }
+                    allFile(filter: {sourceInstanceName: {eq: "pages"}}) {
+                      edges {
+                        node {
+                          name
+                        }
+                      }
+                    }
+                }`,
+                resolvePages: ({
+                    allSitePage: { nodes: sitePages },
+                    allFile: { edges: pageFiles },
+                }) => {
+                    return sitePages.map((page) => {
+                        const pageFile = pageFiles.find(({ node }) => {
+                            const fileName =
+                                node.name === "index" ? "/" : `/${node.name}/`;
+                            return page.path === fileName;
+                        });
+
+                        return { ...page, ...pageFile?.node?.fields };
+                    });
+                },
+                serialize: ({ path }) => {
+                    return {
+                        url: path,
+                        lastmod: new Date().toLocaleDateString("en-US"),
+                    };
+                },
+                createLinkInHead: true,
+            },
+        },
+        `gatsby-plugin-robots-txt`,
+    ],
 };
